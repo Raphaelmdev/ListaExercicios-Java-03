4253fdc486a5e811b7f01ffa0f39ed6939a42311
Raphael Martins <137070545+Raphaelmdev@users.noreply.github.com>
2026-03-06 18:40:05 -0300

    Update Exercício01.java

--- Lista 5.1 - JAVA/Exercício01.java
+++ Lista 5.1 - JAVA/Exercício01.java
@@ -5,7 +5,7 @@ public class Exercício01 {
 
         //1) Desenvolver um programa que exiba os números de 1 a 100. 
     
-                int i = 0;
+                int i = 1;
 
                 while(i <= 100 ){
                 System.out.println(i);
